c52670a8c3674ff6510e93d0f880c3ef1ae77d83
christianburkard <51155407+christianburkard@users.noreply.github.com>
2019-10-22 17:12:48 +0200

    Update seria_test.py

--- SerialConnection/seria_test.py
+++ SerialConnection/seria_test.py
@@ -1,49 +1,60 @@
-
+from tkinter import *
+import tkinter as tk
 import serial
+import cv2
 import time
 
-s = serial.Serial('/dev/ttyUSB0', 9600) # Namen ggf. anpassen
-#s.open()
-time.sleep(5) # der Arduino resettet nach einer Seriellen Verbindung, daher muss kurz gewartet werden
 
-s.write("test")
-try:
-    while True:
-        response = s.readline()
-        print(response)
-except KeyboardInterrupt:
-    s.close()
+class GUI:
+    def __init__(self,root):
 
-#have to run 'sudo apt-get install python-smbus'
+        self.setPnt = tk.Label(root, text="Set point ")
+        self.setPnt.pack(side=LEFT)
 
-#
-## display system info
-#print(os.uname())
-#
-#bus = smbus.SMBus(1)
-#
-## I2C address of Arduino Slave
-#i2c_address = 0x07
-#i2c_cmd = 0x01
-#
-#def ConvertStringToBytes(src):
-#    converted = []
-#    for b in src:
-#        converted.append(ord(b))
-#    return converted
-#
-## send welcome message at start-up
-#bytesToSend = ConvertStringToBytes("Hello Uno")
-#bus.write_i2c_block_data(i2c_address, i2c_cmd, bytesToSend)
-#
-## loop to send message
-#exit = False
-#while not exit:
-#    r = raw_input('Enter something, "q" to quit"')
-#    print(r)
-#
-#    bytesToSend = ConvertStringToBytes(r)
-#    bus.write_i2c_block_data(i2c_address, i2c_cmd, bytesToSend)
-#
-#    if r=='q':
-#        exit=True
+        self.spinBoxZ = tk.Spinbox(root, from_ =-24, to=24,textvariable = var ,command = self.printValues)
+        self.spinBoxZ.pack(side=LEFT)
+
+        self.setBtn = tk.Button(root, text = "Set", command = self.printValues)
+        self.setBtn.pack(side=LEFT)
+
+        self.setConn = tk.Button(root, text = "Connect", command = self.ArduinoConnection)
+        self.setConn.pack(side=LEFT)
+
+#        self.setBtn1 = tk.Button(root, text = "Close", command = root.quit)
+#        self.setBtn1.pack()
+
+    def printValues(self):
+        print("Written: {}".format(self.spinBoxZ.get()))
+#        try:
+        serPC.read(size=100)
+#        serialcmd = input(self.spinBoxZ.get())
+        serPC.write('0'.encode())
+        print("Connection established")
+#        except:
+#        print("Connection failed ...")
+
+    def ArduinoConnection(self):
+            global serPC
+            print("Establishing serial connection between Computer and Arduino ...")
+            try:
+                serPC = serial.Serial('COM10',9600,timeout =0,parity = serial.PARITY_EVEN,rtscts=1)
+                print("Connection established!")
+                return serPC
+            except:
+                print("Connection to Arduino failed")
+
+
+root = tk.Tk()
+var = tk.StringVar(root)
+var.set("0")
+var1 = tk.StringVar()
+var2 = tk.StringVar()
+root.title("Z Position")
+root.geometry('500x100')
+var1.set(0)
+var2.set(0)
+
+
+guiOpen = GUI(root)
+
+root.mainloop()
