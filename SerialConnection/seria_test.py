6e9a452cf5b8818c3bbf1d6d411c6f0eb0fb7e1b
christianburkard <51155407+christianburkard@users.noreply.github.com>
2019-10-22 13:16:29 +0200

    Update seria_test.py

--- SerialConnection/seria_test.py
+++ SerialConnection/seria_test.py
@@ -3,7 +3,7 @@ import serial
 import time
 
 s = serial.Serial('/dev/ttyUSB0', 9600) # Namen ggf. anpassen
-s.open()
+#s.open()
 time.sleep(5) # der Arduino resettet nach einer Seriellen Verbindung, daher muss kurz gewartet werden
 
 s.write("test")
